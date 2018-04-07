52d88fa1f23b1aa26d01603bbc57b97fb4cdddcf
Himavarsha Kagidala <himavarshakagidala@Himavarshas-MacBook-Pro.local>
2018-04-07 00:14:09 -0700

    TST

---
+++ src/main/java/com/lab/tst/App.java
@@ -0,0 +1,14 @@
+package com.lab.tst;
+
+public class App {
+	
+	public static void main(String[] args) {
+		TST tst = new TST();
+		
+		tst.put("apple", 1);
+		tst.put("orange", 2);
+		
+		System.out.println(tst.get("o"));
+	}
+
+}
